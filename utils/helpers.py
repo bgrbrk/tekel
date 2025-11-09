e4c4c8417d9229c663a608e31e2c01d9da613781
bgrbrk <bgrbrk56@gmail.com>
2025-11-09 22:31:51 +0300

    sil

--- utils/helpers.py
+++
@@ -1,5 +0,0 @@
-from decimal import Decimal, ROUND_HALF_UP
-def tl(x):
-    q=Decimal(x).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-    s=f"{q:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-    return f"{s} TL"
